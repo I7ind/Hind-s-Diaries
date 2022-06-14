177d326f577468771fa145d1ea7217cc07d714cc
I7ind <hind-alsuliman@hotmail.com>
2022-06-14 17:01:46 +0300

    MyDiaries

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function mymorning() {
+    alert('HELLO HELLO ;;  Welcome to my Diaries I Hope you enjoy here..');
+    
+}
+function recipese(){
+    alert('There are no recipes today >_<');
+    
+}
+function load(remove) {
+    remove.remove();
+}
